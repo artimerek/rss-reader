4a8ad193acebdd90b5f3afc1c69afcc5a989fabf
Konrad Rzepliński <konradbiznesu@gmail.com>
2022-03-22 11:59:29 +0100

    Perfomance fix

--- app/src/main/java/pl/artimerek/xml_reader/adapter/FeedAdapter.kt
+++ app/src/main/java/pl/artimerek/xml_reader/adapter/FeedAdapter.kt
@@ -6,8 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import android.widget.TextView
-import pl.artimerek.xml_reader.R
 import pl.artimerek.xml_reader.entry.FeedEntry
 
 class FeedAdapter(context: Context, private val resource: Int, private val apps: List<FeedEntry>) :
@@ -24,17 +22,25 @@ class FeedAdapter(context: Context, private val resource: Int, private val apps:
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         Log.d(TAG, "getView called $position")
 
-        val view = inflater.inflate(resource, parent, false)
+        val view: View
+        val viewHolder: ViewHolder
 
-        val tvName: TextView = view.findViewById(R.id.tvName)
-        val tvArtist: TextView = view.findViewById(R.id.tvArtist)
-        val tvSummary: TextView = view.findViewById(R.id.tvSummary)
+        if (convertView == null) {
+            Log.d(TAG, "getView called with null convertView")
+            view = inflater.inflate(resource, parent, false)
+            viewHolder = ViewHolder(view)
+            view.tag = viewHolder
+        } else {
+            Log.d(TAG, "getView provided a convertView")
+            view = convertView
+            viewHolder = view.tag as ViewHolder
+        }
 
         val currentApp = apps[position]
 
-        tvName.text = currentApp.name
-        tvArtist.text = currentApp.artist
-        tvSummary.text = currentApp.summary
+        viewHolder.tvName.text = currentApp.name
+        viewHolder.tvArtist.text = currentApp.artist
+        viewHolder.tvSummary.text = currentApp.summary
 
         return view
     }
